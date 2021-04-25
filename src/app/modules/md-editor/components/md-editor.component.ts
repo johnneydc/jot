f4c6aba268c48aab1c9bee5ddc0f76a526696c1d
Johnney Rey Dela Cruz <johnney@outlook.com>
2021-04-25 13:09:02 +0800

    added enhancements to editor

--- src/app/modules/md-editor/components/md-editor.component.ts
+++ src/app/modules/md-editor/components/md-editor.component.ts
@@ -1,8 +1,13 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'rt-editor',
-  templateUrl: './md-editor.component.html'
+  selector: 'md-editor',
+  templateUrl: './md-editor.component.html',
+  styles: [`
+    .rt-editor-input {
+      white-space: pre-wrap;
+    }
+  `]
 })
 export class MdEditorComponent implements AfterViewInit {
 
@@ -10,13 +15,50 @@ export class MdEditorComponent implements AfterViewInit {
   private readonly editor!: ElementRef<HTMLDivElement>;
 
   ngAfterViewInit() {
-    this.editor.nativeElement.focus();
+    this.editor.nativeElement.addEventListener('paste', ev => {
+      // cancel paste
+      ev.preventDefault();
+
+      // get text representation of clipboard
+      const text = ev.clipboardData.getData('text/plain');
+
+      // insert text manually
+      document.execCommand('insertHTML', false, text);
+    }, { passive: false });
+
+    this.editor.nativeElement.addEventListener('focus', ev => {
+      this.setCaretPosition();
+    });
+
     this.editor.nativeElement.addEventListener('keyup', ev => {
       const el = ev.target as HTMLDivElement;
 
-      if (ev.key === 'Enter') {
-        
+      if (ev.code === 'Space') {
+        // this.setCaretPosition();
       }
     });
+
+    this.editor.nativeElement.addEventListener('keydown', ev => {
+      if (ev.key === 'Tab') {
+        ev.preventDefault();
+        document.execCommand('insertHTML', false, '&nbsp;&nbsp;');
+        this.editor.nativeElement.focus();
+      }
+    }, { passive: false });
+
+    this.setCaretPosition();
+  }
+
+  private setCaretPosition(atStart = false) {
+    const el = this.editor.nativeElement;
+
+    el.focus();
+
+    const range = document.createRange();
+    range.selectNodeContents(el);
+    range.collapse(atStart);
+    const sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
   }
 }
